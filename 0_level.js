a36bee22f2751e36671de814e340785c91f3d9a4
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-11-29 21:19:29 -0300

    level 0 solution

---
+++ 0_level.js
@@ -0,0 +1,14 @@
+// => Player address 0x0718eccf37de50e31620C658B2c96adDc1e76300
+// => Ethernaut address 0xD991431D8b033ddCb84dAD257f4821E9d5b38C33
+// => Instance address 0x033bFa826c915B27aE59ec1bfAe714590e37d9A2
+
+// solution
+await contract.info();
+await contract.info1();
+await contract.info2('hello');
+await contract.infoNum();
+await contract.info42();
+await contract.theMethodName();
+await contract.method7123949();
+await contract.password();
+await contract.authenticate('ethernaut0');
