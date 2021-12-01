7bfde3ecc9978abb93e1c6db3cfa473f42e97054
nicolaslazzos <nicolaslazzos09@gmail.com>
2021-11-30 22:28:07 -0300

    level 2 solution

---
+++ 2_level.js
@@ -0,0 +1,6 @@
+// => Player address 0x0718eccf37de50e31620C658B2c96adDc1e76300
+// => Ethernaut address 0xD991431D8b033ddCb84dAD257f4821E9d5b38C33
+// => Instance address 0x6cfbd108CdA5c28a2386BCa089c44102898A8290
+
+// solution
+await contract.Fal1out();
